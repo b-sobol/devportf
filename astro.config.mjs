25f451c4c952a7cbb5f20be6cdd93b38d8a6735d
Bartłomiej Sobol <bartl.sobol@gmail.com>
2025-09-02 21:57:46 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://b-sobol.github.io",
-  base: "/devportf/",
-  integrations: [tailwind()],
+  site: 'https://b-sobol.github.io',
+  base: '/devportf/',
+  plugins: [tailwindcss()],
 });
